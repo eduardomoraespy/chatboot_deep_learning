e5a1abdabd1e725892c676a0d501b4fe8117a7bd
Eduardo Moraes <eduardomoraes2213@hotmail.com>
2022-02-26 22:46:51 -0300

    setup configurado -- Tudo pronto para começar

---
+++ extract.py
@@ -0,0 +1,23 @@
+import random
+import numpy as np
+import pickle
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+
+lemmatizer = WordNetLemmatizer()
+
+
+def clear_writing():
+    pass
+
+def bag_of_words():
+    pass
+
+def class_prediction():
+    pass
+
+def get_response():
+    pass
+
+print('setup base concluído!')
